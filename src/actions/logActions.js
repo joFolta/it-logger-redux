dae241fdffb07c5ee5f0f95aebc85c15ad792daf
Johann Folta <jfolta@kyruus.com>
2020-06-16 13:00:16 -0400

    logs reducer, actions, and types

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,8 +1,23 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 
-export const getLogs = () => {
-  return async (dispatch) => {
-    // redux-thunk middleware allows async functions inside actions, so we can wait for a response, then dispatch to reducer
+// export const getLogs = () => {
+//   return async (dispatch) => {
+//     // redux-thunk middleware allows async functions inside actions, so we can wait for a response, then dispatch to reducer
+//     setLoading();
+//     const res = await fetch("./logs");
+//     const data = await res.json();
+
+//     dispatch({
+//       type: GET_LOGS,
+//       payload: data,
+//     });
+//   };
+// };
+
+// Refactored with error handling
+export const getLogs = () => async (dispatch) => {
+  // redux-thunk middleware allows async functions inside actions, so we can wait for a response, then dispatch to reducer
+  try {
     setLoading();
     const res = await fetch("./logs");
     const data = await res.json();
@@ -11,7 +26,12 @@ export const getLogs = () => {
       type: GET_LOGS,
       payload: data,
     });
-  };
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
 };
 
 // Set loading to true
